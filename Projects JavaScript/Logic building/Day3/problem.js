7441e0951bdbb736e713c4768fb1c9e53a5f416a
hashimmalik59 <hm3630450@gmail.com>
2025-09-19 20:33:06 +0500

    Day1 of API: Object, Array, map, filter, reduce, JSON stringify() and parse()

---
+++ Projects JavaScript/Logic building/Day3/problem.js
@@ -0,0 +1,25 @@
+// 1. Tutorials band karo - bilkul nahi dekhna!
+// 2. Daily sirf yeh karo:
+
+// 1 problem solve karo(without Google / tutorial)
+// Error aaye to khud fix karo
+// Stuck hoge to 15 min soch kar hi help lena
+
+// 3. Pehle 5 din yeh problems karo:
+
+// Day 1: User se 2 number input leke add karo
+// Day 2: Even / odd check karo
+// Day 3: 1 to 10 numbers print karo
+// Day 4: Sabse bara number nikalo(3 mein se)
+// Day 5: Simple calculator(+-*/) banao
+
+// 4. Rules:
+
+// Khud likhna hai, copy nahi karna
+// Errors se darna nahi
+// Tutorial dekhne ka mann kare to resist karo
+
+// Day 3: 1 to 10 numbers print karo
+for (let i = 1; i <= 10; i++) {
+  console.log(`Number: ${i}`);
+}
